f1bd035dc5f8c124e14f216e6d0e968952614ce6
sleeplesseditor <josh@bryson.me.uk>
2019-02-18 23:25:52 +0000

    Main component setup and demo images added

---
+++ src/components/MemeMaker.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+const photos = [
+    { src: '../demo_images/One-Does-Not-Simply.jpg' },
+    { src: '../demo_images/Socially-Awesome-Awkward-Penguin.jpg' },
+    { src: '../demo_images/fry.jpeg' },
+    { src: '../demo_images/ned.jpg' },
+    { src: '../demo_images/victory-baby.jpg' },
+];
+
+const initialState = {
+    toptext: "", 
+    bottomtext: "", 
+    isTopDragging: false, 
+    isBottomDragging: false, 
+    topY: "10%",  
+    topX: "50%",
+    bottomX: "50%",
+    bottomY: "90%"
+}
+
+class MemeMaker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentImage: 0,
+            modalIsOpen: false,
+            currentImagebase64: null,
+            ...initialState
+        };
+    }
+
+    getBase64Image(img) {
+        // Convert image to data URI
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+        var dataURL = canvas.toDataURL("image/png");
+        return dataURL;
+    }
+
+    openImage = (index) => {
+        const image = photos[index];
+        const base_image = new Image();
+        base_image.src = image.src;
+        const currentImagebase64 = this.getBase64Image(base_image);
+        // Setting the currently selected image on the state.
+        this.setState(prevState => ({
+            currentImage: index,
+            modalIsOpen: !prevState.modalIsOpen,
+            currentImagebase64,
+            ...initialState
+        }));
+    }
+
+    render() {
+        return (
+            <div className="content">
+                {photos.map((image, index) => (
+                    <div className="image-holder" key={image.src}>
+                        <span className="meme-top-caption">Top text</span>
+                        <img 
+                            style={{
+                                width: "100%",
+                                cursor: "pointer",
+                                heght: "100%"
+                            }}
+                            alt={index}
+                            src={image.src}
+                            onClick={() => this.openImage(index)}
+                            role="presentation"
+                        />
+                        <span className="meme-bottom-caption">Bottom text</span>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default MemeMaker;
